88aaed7ede0ab333844e0b3f9b7b00eb4fb0c892
Gongchaeyeon <dnjftjd53@naver.com>
2023-05-25 20:03:43 +0900

    Chore: Controller 환경세팅

--- Controller.py
+++ Controller.py
@@ -2,6 +2,29 @@ import LaundrySystem
 import ManageSystem
 import time
 
+
 class Controller:
-    pass
-    # 초기 변수 선언
+
+    @classmethod
+    def SelectCourse(self):
+        pass
+
+    @classmethod
+    def SelectDetails(self):
+        pass
+
+    @classmethod
+    def StopWashing(self):
+        pass
+
+    @classmethod
+    def ManagementLogin(self):
+        pass
+
+    @classmethod
+    def Inventory(self):
+        pass
+
+    @classmethod
+    def SalesCheck(self):
+        pass
